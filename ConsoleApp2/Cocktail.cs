ca569d9bf37c5329780d848c02f446f584e63fdb
User <User@DESKTOP-79KNQK3>
2025-12-25 13:01:37 +0500

    Голощапов Сергей 3пк2 ШаблонныйМетод Зачет

---
+++ ConsoleApp2/Cocktail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Cocktail : Beverage
+    {
+        protected override void Brew()
+        {
+            Console.WriteLine("Смешиваем ингридиенты в стакане");
+        }
+
+        protected override void AddCondiments()
+        {
+            Console.WriteLine("Добавляем молоко и сливки и трубочку");
+        }
+    }
+}
